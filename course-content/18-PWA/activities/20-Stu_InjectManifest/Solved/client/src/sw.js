02ca3325a0f33e648aec80513c6dc21dbbaa5dd5
kendrick Brown <chefken360@gmail.com>
2022-12-10 15:27:51 -0800

    bootcamp notes

---
+++ course-content/18-PWA/activities/20-Stu_InjectManifest/Solved/client/src/sw.js
@@ -0,0 +1,76 @@
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+
+// Import the expiration plugin
+import { ExpirationPlugin } from 'workbox-expiration';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const cacheName = 'static-resources';
+// The destination read-only property of the Request interface returns a string describing the type of content being requested.
+// https://developer.mozilla.org/en-US/docs/Web/API/Request/destination
+const matchCallback = ({ request }) => {
+  console.log(request);
+  return (
+    // CSS
+    request.destination === 'style' ||
+    // JavaScript
+    request.destination === 'script'
+  );
+};
+
+registerRoute(
+  matchCallback,
+  new StaleWhileRevalidate({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// Register route for caching images
+// The cache first strategy is often the best choice for images because it saves bandwidth and improves performance.
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'my-image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+
+// const matchCb = ({url, request, event}) => {
+//   return url.pathname === '/www.omdbapi.com/';
+// };
+
+
+// registerRoute(
+//   matchCb,
+//   new CacheFirst({
+//     cacheName: 'my-json-cache',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//       new ExpirationPlugin({
+//         maxEntries: 60,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   })
+// );
+
+
---
+++ course-content/19-React/activities/10-Stu_Component/Unsolved/src/components/List.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// function List(props) {
+//   return (
+//     // <ul className="list-group">
+//       {/* TODO: Use the `map` method to iterate over each grocery and return a `li` element that contains the item's name and has a unique key attribute */}
+//       {/* {props.groceries.map((item) => {
+//         return (
+//           // TODO: Your code here
+//           // {user.map((user) => (
+//           //   <li className = "list-group-item" key={user.login.uuid}>
+              
+//           //   </li>
+//           ))}
+//         );
+//       })}
+//     </ul>
+//   );
+// } */}
+
+// export default List;
